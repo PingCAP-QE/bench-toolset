4a5ebdb7f413c755e3bfe284ff18213d3899db65
5kbpers <tangminghua@pingcap.com>
2020-11-12 16:52:18 +0800

    cmd: fix connecting record database
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- cmd/bench.go
+++ cmd/bench.go
@@ -6,7 +6,8 @@ import (
 
 	"github.com/5kbpers/stability_bench/bench"
 	"github.com/5kbpers/stability_bench/workload"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
+	"github.com/pingcap/errors"
 	"github.com/pingcap/log"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
@@ -31,7 +32,7 @@ var (
 func init() {
 	benchCmd := NewBenchCommand()
 
-	benchCmd.PersistentFlags().StringVar(&host, "host", "localhost", "host of tidb cluster")
+	benchCmd.PersistentFlags().StringVar(&host, "host", "127.0.0.1", "host of tidb cluster")
 	benchCmd.PersistentFlags().StringVar(&user, "user", "root", "username of tidb cluster")
 	benchCmd.PersistentFlags().StringVar(&db, "db", "test", "database of tidb cluster")
 	benchCmd.PersistentFlags().StringVar(&logPath, "log", "", "log path of workload")
@@ -46,12 +47,6 @@ func NewBenchCommand() *cobra.Command {
 	command := &cobra.Command{
 		Use:   "bench",
 		Short: "Run benchmarks for stability test",
-		PreRunE: func(cmd *cobra.Command, args []string) error {
-			var err error
-			log.Info("Connect to record database...", zap.String("dsn", recordDbDsn))
-			recordDb, err = sql.Open("mysql", recordDbDsn)
-			return err
-		},
 	}
 
 	command.AddCommand(newGcBenchCommand())
@@ -66,16 +61,31 @@ func newGcBenchCommand() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var err error
 			var benchId int64
-			if recordDb != nil {
-				log.Info("Create a record for this benchmark...")
-				var rs sql.Result
-				rs, err = recordDb.Exec(`INSERT INTO bench_info ("name") VALUES ("gc")`)
+			if len(recordDbDsn) != 0 {
+				config ,err := mysql.ParseDSN(recordDbDsn)
 				if err != nil {
-					return err
+					return errors.Trace(err)
 				}
+				log.Info("Parse record database DSN", zap.Reflect("config", config))
+				log.Info("Connect to record database...")
+				recordDb, err = sql.Open("mysql", recordDbDsn)
+				if err != nil {
+					return errors.Trace(err)
+				}
+				err = recordDb.Ping()
+				if err != nil {
+					return errors.Trace(err)
+				}
+				var rs sql.Result
+				log.Info("Create a record for this benchmark...")
+				rs, err = recordDb.Exec(`INSERT INTO bench_info (name) VALUES ("gc")`)
+				if err != nil {
+					return errors.Trace(err)
+				}
+				log.Info("Get benchmark id...", zap.Int64("id", benchId))
 				benchId, err = rs.LastInsertId()
 				if err != nil {
-					return err
+					return errors.Trace(err)
 				}
 			}
 
@@ -96,23 +106,23 @@ func newGcBenchCommand() *cobra.Command {
 			log.Info("Prepare benchmark...")
 			err = b.Prepare()
 			if err != nil {
-				return err
+				return errors.Trace(err)
 			}
 			log.Info("Start to run benchmark...")
 			err = b.Run()
 			if err != nil {
-				return err
+				return errors.Trace(err)
 			}
 			results, err := b.Results()
 			if err != nil {
-				return err
+				return errors.Trace(err)
 			}
 			log.Info("Benchmark done, save results to record database...", zap.Reflect("results", results))
 			if recordDb != nil {
 				for _, rs := range results {
-					_, err = recordDb.Exec("INSERT INTO bench_record values (?, ?, ?)", benchId, rs.Name, rs.Value)
+					_, err = recordDb.Exec("INSERT INTO bench_result values (?, ?, ?)", benchId, rs.Name, rs.Value)
 					if err != nil {
-						return err
+						return errors.Trace(err)
 					}
 				}
 			}
