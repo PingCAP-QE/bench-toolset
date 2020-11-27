de70862864de572cc8854f4de18171e5e1f4043c
mahjonp <junpeng.man@gmail.com>
2020-11-27 21:41:12 +0800

    update sysbench summary report
    Signed-off-by: mahjonp <junpeng.man@gmail.com>

--- workload/sysbench.go
+++ workload/sysbench.go
@@ -14,6 +14,12 @@ import (
 
 var (
 	sysbenchRecordRegexp = regexp.MustCompile(`\[\s(\d+s)\s\]\sthds:\s(\d+)\stps:\s([\d\.]+)\sqps:\s[\d\.]+\s[\(\)\w/:\s\d\.]+\slat\s\(ms,(\d+)%\):\s([\d\.]+)`)
+	sysbenchTpsRegex     = regexp.MustCompile(`\n\s+transactions:\s+([\d.]+)\s*\(([\d.]+)\s+per\s+sec\.\)`)
+	sysbenchQpsRegex     = regexp.MustCompile(`\n\s+queries:\s+([\d.]+)\s*\(([\d.]+)\s+per\s+sec\.\)`)
+	sysbenchMinLatRegex  = regexp.MustCompile(`\n\s+min:\s+([\d.]+)`)
+	sysbenchAvgLatRegex  = regexp.MustCompile(`\n\s+avg:\s+([\d.]+)`)
+	sysbenchMaxLatRegex  = regexp.MustCompile(`\n\s+max:\s+([\d.]+)`)
+	sysbenchP99LatRegex  = regexp.MustCompile(`\n\s+99th percentile:\s+([\d.]+)`)
 )
 
 type Sysbench struct {
@@ -64,6 +70,7 @@ func ParseSysbenchRecords(logPath string) ([]*Record, []*Record, error) {
 	}
 	matchedRecords := sysbenchRecordRegexp.FindAllSubmatch(content, -1)
 	records := make([]*Record, len(matchedRecords))
+	summaryRecords := make([]*Record, 0)
 	for i, matched := range matchedRecords {
 		threads, err := strconv.ParseFloat(string(matched[2]), 64)
 		if err != nil {
@@ -95,8 +102,55 @@ func ParseSysbenchRecords(logPath string) ([]*Record, []*Record, error) {
 			records[i].P99LatInMs = p99Lat
 		}
 	}
-
-	return records, nil, nil
+	tps := sysbenchTpsRegex.FindAllSubmatch(content, -1)
+	if len(tps) == 1 {
+		summaryRecords = append(summaryRecords, &Record{
+			Type:    "summary",
+			Tag:     "tps",
+			Payload: string(tps[0][2]),
+		})
+	}
+	qps := sysbenchQpsRegex.FindAllSubmatch(content, -1)
+	if len(qps) == 1 {
+		summaryRecords = append(summaryRecords, &Record{
+			Type:    "summary",
+			Tag:     "qps",
+			Payload: string(qps[0][2]),
+		})
+	}
+	minLat := sysbenchMinLatRegex.FindAllSubmatch(content, -1)
+	if len(minLat) == 1 {
+		summaryRecords = append(summaryRecords, &Record{
+			Type:    "summary",
+			Tag:     "minLat",
+			Payload: string(minLat[0][1]),
+		})
+	}
+	avgLat := sysbenchAvgLatRegex.FindAllSubmatch(content, -1)
+	if len(avgLat) == 1 {
+		summaryRecords = append(summaryRecords, &Record{
+			Type:    "summary",
+			Tag:     "avgLat",
+			Payload: string(avgLat[0][1]),
+		})
+	}
+	maxLat := sysbenchMaxLatRegex.FindAllSubmatch(content, -1)
+	if len(maxLat) == 1 {
+		summaryRecords = append(summaryRecords, &Record{
+			Type:    "summary",
+			Tag:     "maxLat",
+			Payload: string(maxLat[0][1]),
+		})
+	}
+	p99Lat := sysbenchP99LatRegex.FindAllSubmatch(content, -1)
+	if len(p99Lat) == 1 {
+		summaryRecords = append(summaryRecords, &Record{
+			Type:    "summary",
+			Tag:     "p99Lat",
+			Payload: string(p99Lat[0][1]),
+		})
+	}
+	return records, summaryRecords, nil
 }
 
 func (s *Sysbench) buildArgs() []string {
