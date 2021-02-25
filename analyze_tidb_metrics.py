d31e1d6f330803a049dea44cf30ad2096911caf8
5kbpers <tangminghua@pingcap.com>
2021-02-25 17:42:08 +0800

    metric: fix report error
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- analyze_tidb_metrics.py
+++ analyze_tidb_metrics.py
@@ -1,14 +1,15 @@
 import argparse
 from analyze.metrics import Connection
 from datetime import datetime
+from analyze.timeseries import TimeSeries
 
 
 def parse_args():
     p = argparse.ArgumentParser(description="Analyze jitter from TiDB metrics")
 
     p.add_argument("--url", type=str, default="http://127.0.0.1:9090", help="Url for connecting Prometheus")
-    p.add_argument("--start", type=int, default=0, help="Timestamp in seconds")
-    p.add_argument("--end", type=int, default=0, help="Timestamp in seconds")
+    p.add_argument("--start", type=int, default=0, help="Timestamp in milliseconds")
+    p.add_argument("--end", type=int, default=0, help="Timestamp in milliseconds")
 
     return p.parse_args()
 
@@ -16,8 +17,10 @@ def parse_args():
 if __name__ == '__main__':
     args = parse_args()
     connect = Connection(args.url)
-    print(args.start, args.end)
-    start_time = datetime.fromtimestamp(int(args.start/1000))
-    end_time = datetime.fromtimestamp(int(args.end/1000))
+    start_time = datetime.fromtimestamp(int(args.start / 1000))
+    end_time = datetime.fromtimestamp(int(args.end / 1000))
     results = connect.tidb_qps(start_time, end_time)
-    print(results)
+    time, values = zip(*map(lambda x: (datetime.fromtimestamp(x[0]).strftime("%Y-%m-%d %H:%M:%S"), float(x[1])),
+                            results[0]["values"]))
+    eval_result = TimeSeries(list(values)).eval(time)
+    print('\n'.join(["{:20s}\t{:20s}".format(x, y) for x, y in eval_result.items()]))
